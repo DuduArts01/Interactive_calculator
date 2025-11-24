cc9abec5a4e41f68210feb46302925a6e79fabd1
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2025-11-24 10:23:05 -0300

    Refactor logic for operator selection and answer checking

--- onevscomputer/logic.py
+++ onevscomputer/logic.py
@@ -1,62 +1,62 @@
-from random import randint, choice
+from random import randint
 
 class Logic_calculator:
     def __init__(self):
-        self.last_denominators = []  # saves the last used numbers
-        # create list of possible denominators, excluding the last ones
-        self.candidates = [i for i in range(1, 11) if i not in self.last_denominators[-3:]]
-    
+        self.choose = None
+        self.n1 = 0
+        self.n2 = 0
+
     def randomNumber(self):
-        self.n1 = 1
-        self.n2 = 2 
+        """Escolhe dois números aleatórios iniciais."""
+        self.n1 = randint(0, 50)
+        self.n2 = randint(0, 50)
 
     def choose_operator(self):
-        self.choose = randint(0,3)
-        
-        # Easy calculate numbers to user
-        if self.choose == 3: # 3 
-            while self.n1 % self.n2 != 0: # Answer is interger and n1 >= n2 
-                self.n1 = randint(0,100) 
-                self.n2 = randint(1,10) # Can never have denominator 0 
-            return "/"
-            
-        
-        
-        elif self.choose == 2: # 2
-            while self.n1 < self.n2: # Answer is interger and n1 >= n2
-                self.n1 = randint(0,10)
-                self.n2 = randint(0,9)
+        """Escolhe o operador entre +, -, x, / garantindo resultados inteiros quando necessário."""
+        self.choose = randint(0, 3)
 
-            return "x" 
-        
-        elif self.choose: # 1
-            while self.n1 < self.n2: # Answer is interger and n1 >= n2
-                self.n1 = randint(0,50)
-                self.n2 = randint(0,49)
-            
+        if self.choose == 0:
+            # Soma
+            return "+"
+
+        elif self.choose == 1:
+            # Subtração (garante n1 >= n2)
+            while self.n1 < self.n2:
+                self.n1 = randint(0, 50)
+                self.n2 = randint(0, 50)
             return "-"
-        
-        else: # 0
-            while self.n1 < self.n2: # Answer is interger and n1 >= n2
-                self.n1 = randint(0,50)
-                self.n2 = randint(0,49)
-            
-            return "+"         
+
+        elif self.choose == 2:
+            # Multiplicação
+            return "x"
+
+        else:
+            # Divisão inteira
+            while True:
+                self.n1 = randint(1, 100)
+                self.n2 = randint(1, 10)
+                if self.n1 % self.n2 == 0:  # garante resultado inteiro
+                    break
+            return "/"
+
+    def get_correct_answer(self):
+        """Retorna o resultado correto com base no operador."""
+        if self.choose == 0:
+            return self.n1 + self.n2
+        elif self.choose == 1:
+            return self.n1 - self.n2
+        elif self.choose == 2:
+            return self.n1 * self.n2
+        elif self.choose == 3:
+            return self.n1 / self.n2
+
+    def is_correct(self, user_answer):
+        """Retorna True/False se o usuário acertou."""
+        return user_answer == self.get_correct_answer()
 
     def checknumber(self, answerUser):
-        if self.choose == 3 and answerUser == (self.n1 / self.n2): # Correct Sum
+        """Mantém compatibilidade com seu código antigo."""
+        if self.is_correct(answerUser):
             return "Parabéns!"
-        elif self.choose == 3 and answerUser != (self.n1 / self.n2): # Wrong Sum
-            return "Errou! Tente de novo!" 
-        elif self.choose == 2 and answerUser == (self.n1 * self.n2): # Correct Subtration
-            return "Parabéns!"
-        elif self.choose == 2 and answerUser != (self.n1 * self.n2): # Wrong Subtration
-            return "Errou! Tente de novo!"
-        elif self.choose and answerUser == (self.n1 - self.n2): # Correct multiply
-            return "Parabéns!"
-        elif self.choose and answerUser != (self.n1 - self.n2): # Wrong multiply
-            return "Errou! Tente de novo!"
-        elif not self.choose and answerUser == (self.n1 + self.n2): # Correct Division
-            return "Parabéns!"
-        elif not self.choose and answerUser != (self.n1 + self.n2): # Wrong Division
+        else:
             return "Errou! Tente de novo!"
